9ef89a42ffbdb70125d09499bd00046020f9f74f
Agent-grey <markkibuthu@gmail.com>
2024-09-02 21:37:05 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+
+
+        anagrams = []
+        for candidate in word_list:
+            
+            if sorted(candidate) == sorted_word:
+                anagrams.append(candidate)
+
+        return anagrams
